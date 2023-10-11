716e3614bea54eb97a00e1b006a0037fbc2b6444
EsliJanai <142496949+EsliJanai@users.noreply.github.com>
2023-10-11 07:28:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let show = true;
+const menuContent = document.querySelector('.content');
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+
+menuToggle.addEventListener('click', () => {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    menuContent.classList.toggle('on', show);
+    show = !show;
+})
+
+// Selecione os elementos relevantes
+const pedirButtons = document.querySelectorAll('.pedir');
+const carrinhoList = document.querySelector('.itens-carrinho');
+
+// Adicione um evento de clique aos botões "pedir"
+pedirButtons.forEach((button) => {
+  button.addEventListener('click', (tipo) => {
+    const item = button.parentNode;
+    const itemName = document.getElementById(`${tipo}`).value;
+    const itemPrice = item.querySelector('h4 span').textContent;
+    // Crie um novo elemento para o carrinho
+    const carrinhoItem = document.createElement('li');
+    carrinhoItem.textContent = `${itemName} - ${itemPrice}`;
+    
+    // Adicione o item ao carrinho
+    carrinhoList.appendChild(carrinhoItem);
+  });
+});
+
+const buttonJs = () => {
+var x = document.getElementById("cart");
+x.style.display = "none"; 
+
+}
+
+// Ajustes no codigo
+const adicionaEntrada = (tipo) => {
+  var x = document.getElementById("cart");
+  x.style.display = "flex"; 
+  console.log('t', tipo);
+  const itemName = document.getElementById(`${tipo}`).value;
+  //const itemPrice = item.querySelector('h4 span').textContent;
+  console.log('r', itemName);
+  // Crie um novo elemento para o carrinho
+  const carrinhoItem = document.createElement('li');
+  carrinhoItem.textContent = `${itemName} -`;
+  
+  // Adicione o item ao carrinho
+  carrinhoList.appendChild(carrinhoItem);
+}
